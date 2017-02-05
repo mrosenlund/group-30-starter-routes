f9546ae7eab3498c2ca56cad3a64e575cdf4e5ce
ashayegh <shayegh.software@gmail.com>
2017-02-05 15:06:03 -0800

    Closes #10 - Adds 404 page

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,7 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'Hogwarts';
-$route['404_override'] = '';
+$route['404_override'] = 'Hogwarts/randomize';
 $route['translate_uri_dashes'] = FALSE;
 $route['sleep'] = 'first/zzz';
 $route['show/(:num)'] = 'first/gimme/$1';
