ddde0b757dcc4d5d18984b20d288bb13e5b8a657
mrosenlund <mrosenlund@my.bcit.ca>
2017-02-05 11:22:41 -0800

    Closes #9 - Change default controller to Hogwarts

--- application/controllers/Bingo.php
+++ application/controllers/Bingo.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends Application
+class Bingo extends Application
 {
 
 	function __construct()
@@ -16,16 +16,13 @@ class Welcome extends Application
 	public function index()
 	{
 		// this is the view we want shown
-		$this->data['pagebody'] = 'homepage';
+		$this->data['pagebody'] = 'justone';
 
-		// build the list of authors, to pass on to our view
-		$source = $this->quotes->all();
-		$authors = array ();
-		foreach ($source as $record)
-		{
-			$authors[] = array ('who' => $record['who'], 'mug' => $record['mug'], 'href' => $record['where']);
-		}
-		$this->data['authors'] = $authors;
+		//get author
+		$record = $this->quotes->get(5);
+
+		//merge
+		$this->data = array_merge($this->data, $record);
 
 		$this->render();
 	}
