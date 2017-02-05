1d00f942069fc308cb5d5227b536033a55731bd5
ashayegh <shayegh.software@gmail.com>
2017-02-05 14:43:27 -0800

    CLoses #4 - Fixed second image

---
+++ application/controllers/welcome.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shayegh
+ * Date: 2/5/17
+ * Time: 2:40 PM
+ */
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Welcome extends Application
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+    /**
+     * Homepage for our app
+     */
+    public function index()
+    {
+        // this is the view we want shown
+        $this->data['pagebody'] = 'homepage';
+        // build the list of authors, to pass on to our view
+        $source = $this->quotes->all();
+        $authors = array ();
+        foreach ($source as $record)
+        {
+            $authors[] = array ('who' => $record['who'], 'mug' => $record['mug'], 'href' => $record['where']);
+        }
+        $this->data['authors'] = $authors;
+        $this->render();
+    }
+
+    public function shucks() {
+        $this->data['pagebody'] = 'justone';
+        $record = $this->quotes->second();
+        $this->data = array_merge($this->data, $record);
+
+        $this->render();
+    }
+}
